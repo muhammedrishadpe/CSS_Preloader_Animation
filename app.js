75c19078522cdd83fb8a57f1702b21ea94e5dcbc
muhammedrishadpe <rishadpe@gmail.com>
2021-06-19 11:23:01 +0530

    updated

---
+++ app.js
@@ -0,0 +1,7 @@
+//Need to make a funcation that gets rid of the preloader
+
+
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add("preload-finish");
+})
